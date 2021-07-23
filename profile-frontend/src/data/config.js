8441340d4e2b5e96954b4a0ef2c44e3c69c33348
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-07-23 13:38:11 +0530

    qprofile added

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Deepanshu Jindal",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Deepanshu Jindal and I'm a Computer Science Student!",
+  githubUrl: "https://github.com/ultimatecoder2",
+  linkedinUrl: "https://www.linkedin.com/in/deepanshu-jindal-16b2a2121/",
+  cvLink: "#",
+};
